219b4b251999750bfe6efd969fa5468e28d6eef9
David Ralph <ohlookitsderpy@protonmail.com>
2019-01-03 20:46:27 +0000

    fax

--- commands/info.js
+++ commands/info.js
@@ -15,7 +15,7 @@ exports.run = (client, message, args) => {
     .addField('__Uptime__', moment.duration(client.uptime).format('d[d ]h[h ]m[m ]s[s]'), true)
     .addField('__Memory Usage__', `${Math.round(process.memoryUsage().heapUsed / 1024 / 1024)}MB`, true)
     .addField('__Prefix__', `\`${config.prefix}\``, true)
-    .addField('__Developer__', '<@484765734803734540>', true)
+    .addField('__Developer__', '<@484765734803734540> <@145557815287611393>', true)
     .addField('__Moe\'s Links__', '[Website](https://etcroot.pw/moe/) | [Github](https://github.com/MoeRadio) | [Invite](https://discordapp.com/api/oauth2/authorize?client_id=523779990798598167&permissions=8&scope=bot) | [Support](https://discord.gg/M6hRKwF)', false)
     .setFooter('Moe | By: etcroot#7884 and ohlookitsderpy#1337')
     .setColor('#A65EA5')
