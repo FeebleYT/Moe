cf9fd8e23307375cb34ae48b6097764dbda40ad9
David Ralph <ohlookitsderpy@protonmail.com>
2019-01-09 12:31:29 +0000

    still wip but here's a thing

--- commands/volume.js
+++ commands/volume.js
@@ -1,15 +1,15 @@
-const Discord = require("discord.js");
+const { RichEmbed } = require("discord.js");
 const ffmpeg = require("ffmpeg-binaries");
 const opusscript = require("opusscript");
 
 exports.run = (client, message, args) => {
      let vol = args.join(" ");
 const voiceConnection = client.voiceConnections.find(val => val.channel.guild.id == message.guild.id);
-if (voiceConnection === null) return message.channel.send('No music being played.');
+if (voiceConnection === null) return message.channel.send('No music is being played!');
 const dispatcher = voiceConnection.player.dispatcher;
 const currentvol = dispatcher.volume * 100;
 if(!vol) {
-    let embed = new Discord.RichEmbed()
+    let embed = new RichEmbed()
     .setDescription(`**Current Volume:** ${currentvol}%`)
     .setColor('#A65EA5')
     return message.channel.send(embed);
@@ -18,7 +18,7 @@ if(!vol) {
 			response.delete(5000);
 		});
 
-        let embed = new Discord.RichEmbed()
+        let embed = new RichEmbed()
         .setDescription("Volume set to " + vol)
         .setColor('#A65EA5')
 		dispatcher.setVolume((vol/100));
